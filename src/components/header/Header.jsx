4619cfb8bb970d3bacc8903d2e9c8baf8c6b45ab
Khoi Nguyen <khoinguyen311@gmail.com>
2022-05-12 01:38:48 -0700

    add profile picture styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import './header.css'
 import CTA from './CTA'
 import Socials from './Socials'
+import Me from '../../images/me.jpg'
+import './header.css'
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, I'm</h5>
+        <h5>Hi hello, I'm</h5>
         <h1>Khoi!</h1>
+        <div className='me'>
+          <img src={Me} alt='Hi hello!' />
+        </div>
         <h5 className="text-light">Software Engineer</h5>
         <CTA />
         <Socials />
