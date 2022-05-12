9ec63bd600daa5a23a86bbdfcb1acc2315dc2a0f
Khoi Nguyen <khoinguyen311@gmail.com>
2022-05-12 14:49:10 -0700

    add spacer between public and private socials

--- src/components/header/Socials.jsx
+++ src/components/header/Socials.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BsLinkedin, BsGithub, BsTwitter, BsInstagram} from 'react-icons/bs'
+import {BsLinkedin, BsGithub, BsTwitter, BsInstagram, BsDot} from 'react-icons/bs'
 
 
 const Socials = () => {
@@ -7,6 +7,7 @@ const Socials = () => {
     <div className='socials'>
       <a href='https://linkedin.com/in/khoifishpond' target='_blank'><BsLinkedin /></a>
       <a href='https://github.com/khoifishpond' target='_blank'><BsGithub /></a>
+      <BsDot color='#4db5ff'/>
       <a href='https://twitter.com/khoifishpond' target='_blank'><BsTwitter /></a>
       <a href='https://instagram.com/khoifishpond' target='_blank'><BsInstagram /></a>
     </div>
